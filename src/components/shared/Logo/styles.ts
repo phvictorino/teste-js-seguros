d3fcc5dcb3aeecf76fc2de904e61efc7ee879c7d
Pedro H. Victorino <pedro.victorino@gmail.com>
2020-10-12 16:08:05 -0400

    feat: styled logo component

---
+++ src/components/shared/Logo/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+import colors from "../../../utils/colors";
+
+export const Image = styled.img`
+  width: auto;
+  height: 100%;
+  color: ${colors.light};
+`;
