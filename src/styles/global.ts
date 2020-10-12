cead8aa84ebdbf5c9cd0f00ea263df7d1bcb8547
Pedro H. Victorino <pedro.victorino@gmail.com>
2020-10-12 18:35:01 -0400

    improvement: improved global style

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,11 +1,14 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+  * {
+    font-family: 'Roboto Mono', monospace;
+  }
+
   body {
     padding: 0;
     margin: 0;
     box-sizing: border-box;
-    font-family: 'Roboto Mono', monospace;
   }
 `;
 
